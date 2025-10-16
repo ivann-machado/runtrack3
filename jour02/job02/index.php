271d808b0c392b12a0d36aef8a2a3d709e994d44
Ivann <ivann.machado@laplateforme.io>
2025-10-16 12:14:47 +0200

    job2

---
+++ jour02/job02/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<script src="script.js"></script>
+
+</head>
+<body>
+<button id="button">Bouton</button>
+
+</body>
+</html>
