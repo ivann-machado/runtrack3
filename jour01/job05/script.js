90bd962ed71097b7f0e8b05e2f83bf5f3bf0e5bc
Ivann <ivann.machado@laplateforme.io>
2025-10-16 09:58:36 +0200

    job5

---
+++ jour01/job05/script.js
@@ -0,0 +1,9 @@
+function displayWeekdays() {
+	const weekdays = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+	weekdays.forEach(day => {
+		console.log(day);
+	});
+}
+
+displayWeekdays();
