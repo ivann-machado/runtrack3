ec8fae4d8cc3ab8049305d3c4401e31854a9c2a2
Ivann <ivann.machado@laplateforme.io>
2025-10-22 10:23:05 +0200

    job2

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,20 +1,7 @@
-function showhide() {
-	let article = document.getElementById("article");
-	if (!article) {
-		article = document.createElement("article");
-		article.id = "article";
-		article.innerHTML = 'L\'important n\'est pas la chute, mais l\'atterrissage.';
-		document.body.appendChild(article);
-	}
-	else {
-		if (article.style.display === "none") {
-			article.style.display = "block";
-		} else {
-			article.style.display = "none";
-		}
-	}
+function jsonValueKey(jsonString, key) {
+	const object = JSON.parse(jsonString);
+	return object[key];
 }
+const jsonString = '{"name": "La Plateforme_", "address": "8 rue d\'hozier", "city": "Marseille", "nb_staff": "11", "creation": "2019"}';
 
-window.addEventListener("load", function() {
-	document.getElementById("button").onclick = showhide;
-});
+console.log(jsonValueKey(jsonString, 'city'));
