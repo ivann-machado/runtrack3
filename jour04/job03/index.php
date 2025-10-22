40cb50569b8924eb2fd9665848b444674d89bda4
Ivann <ivann.machado@laplateforme.io>
2025-10-22 14:33:19 +0200

    job3

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -8,8 +8,44 @@
 
 </head>
 <body>
-<button id="button">Bouton</button>
-<p id="compteur">0</p>
-
+	<form name="pokemon">
+		<label for="id">ID</label>
+		<input type="text" id="id" name="id">
+		<label for="nom">Nom</label>
+		<input type="text" id="nom" name="nom">
+		<label for="type">Type</label>
+		<select id="type" name="type">
+			<option value="Normal">Normal</option>
+			<option value="Fire">Feu</option>
+			<option value="Water">Eau</option>
+			<option value="Grass">Plante</option>
+			<option value="Electric">Electrik</option>
+			<option value="Ice">Glace</option>
+			<option value="Fighting">Combat</option>
+			<option value="Poison">Poison</option>
+			<option value="Ground">Sol</option>
+			<option value="Flying">Vol</option>
+			<option value="Psychic">Psy</option>
+			<option value="Bug">Insecte</option>
+			<option value="Rock">Roche</option>
+			<option value="Ghost">Spectre</option>
+			<option value="Dragon">Dragon</option>
+			<option value="Dark">Ténèbres</option>
+			<option value="Steel">Acier</option>
+			<option value="Fairy">Fée</option>
+		</select>
+		<input type="submit" value="Trier">
+	</form>
+	<table border="1" id="pokemonTable">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Nom</th>
+				<th>Type</th>
+			</tr>
+		</thead>
+		<tbody id="results">
+		</tbody>
+	</table>
 </body>
 </html>
--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -1,9 +1,34 @@
-let counter = 0;
-function addone() {
-	counter++;
-	document.getElementById("compteur").innerText = counter;
-}
-
 window.addEventListener("load", function() {
-	document.getElementById("button").onclick = addone;
+	let pokemons;
+	const resultsTable = document.getElementById("results");
+	fetch("./pokemon.json").then((res) => res.json()).then((data) => {
+		pokemons = data;
+		pokemons.forEach((pokemon) => {
+			const pokemonRow = document.createElement("tr");
+			pokemonRow.innerHTML = `<td>${pokemon.id}</td><td>${pokemon.name.french}</td><td>${pokemon.type.join(", ")}</td>`;
+			resultsTable.appendChild(pokemonRow);
+		});
+	});
+	addEventListener("submit", function(event) {
+		event.preventDefault();
+		const entries = Object.fromEntries(new FormData(event.target));
+
+		const filteredPokemons = pokemons.filter((pokemon) => {
+			return (
+				(!entries.id || pokemon.id == entries.id) &&
+				(!entries.nom || pokemon.name.french.toLowerCase() == entries.nom.toLowerCase()) &&
+				(!entries.type || pokemon.type.includes(entries.type)));
+		});
+		resultsTable.innerHTML = "";
+		if (filteredPokemons.length === 0) {
+			resultsTable.innerHTML = "<tr>Pas de résultat</tr>";
+		}
+		else {
+			filteredPokemons.forEach((pokemon) => {
+				const pokemonRow = document.createElement("tr");
+				pokemonRow.innerHTML = `<td>${pokemon.id}</td><td>${pokemon.name.french}</td><td>${pokemon.type.join(", ")}</td>`;
+				resultsTable.appendChild(pokemonRow);
+			});
+		}
+	});
 });
